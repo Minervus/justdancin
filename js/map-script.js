9a79eac08c214b04ddc87a7698e3dc515c34faea
Tony Nguyen <tnguyen411@gmail.com>
2017-06-28 13:51:47 -0700

    Add files via upload

--- js/map-script.js
+++ js/map-script.js
@@ -16,7 +16,7 @@
   		var mapOptions = {
     		zoom: 18,		
 			mapTypeId: google.maps.MapTypeId.ROADMAP,	
-    		center: new google.maps.LatLng(-12.046014, -77.030503),
+    		center: new google.maps.LatLng(49.2717992,-123.190403),
 			panControl: true,
   			zoomControl: true,
   			mapTypeControl: true,
