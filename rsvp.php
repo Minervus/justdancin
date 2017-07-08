f9f661a46e6991ddb9d82593f332ca2fb882875c
Gary Yau <gyau88@gmail.com>
2017-07-08 16:54:01 -0700

    Fixed merge conflict.

--- rsvp.php
+++ rsvp.php
@@ -19,23 +19,12 @@
 				$finalmessage .= $_POST[$input_id."_label"]." : ". $_POST[$input_id] . "\n\n";
 			}
 		}
-<<<<<<< HEAD
-	
-		$email_to  =  'tonyandamandanguyen@gmail.com'; 
-		
-		$headers = "From: ".$_POST["inputemail"]."\r\n";	
-		$headers .= "Reply-To: ".$_POST["inputemail"]."\r\n";	
-		$subject = "RSVP message from Mr/Mrs ".$_POST["inputname"];	
-				
-=======
-
-		u  =  'gyau88@gmail.com';
+		$email_to  =  'tonyandamandanguyen@gmail.com';
 
 		$headers = "From: ".$_POST["inputemail"]."\r\n";
 		$headers .= "Reply-To: ".$_POST["inputemail"]."\r\n";
 		$subject = "RSVP message from Mr/Mrs ".$_POST["inputname"];
 
->>>>>>> a58efa77765d7750a22b98e183730b06e17bc076
 		if(mail($email_to, $subject, $finalmessage, $headers)){
 			$output = json_encode(array('type'=>'success', 'text' => 'Message Sent'));
 		}else{
